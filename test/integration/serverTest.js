3f97a9b5750280b3f2020ba92e571dfddcbe39e6
IlyaNotebook <ilya.kugaevsky@gmail.com>
2017-12-01 12:01:53 -0500

    Rewrite GET-tests in async-await terms

--- test/integration/serverTest.js
+++ test/integration/serverTest.js
@@ -1,12 +1,13 @@
 //const should = require('should');
 // should(process.env.NODE_ENV).eql('test');
 
-const rp = require('request-promise').defaults({
+const rp = require("request-promise").defaults({
   encoding: null,
-  resolveWithFullResponse: true
+  resolveWithFullResponse: true,
+  simple: false
 });
 
-const should = require('should');
+const should = require("should");
 const fs = require("fs-extra");
 
 const config = require("../../config/default");
@@ -23,34 +24,31 @@ const host = "http://127.0.0.1:3000";
 describe("Server tests", function() {
   let app;
 
-  // const mochaAsync = function(fn) {
-  //   return async function (done) {
-  //     try {
-  //       await fn();
-  //       done();
-  //     } catch (err) {
-  //       done(err);
-  //     }
-  //   };
-  // };
-
-  const smallFileTempPath = path.join(config.testRoot, "fixtures/", smallFileName);
+  const smallFileTempPath = path.join(
+    config.testRoot,
+    "fixtures/",
+    smallFileName
+  );
   const bigFileTempPath = path.join(config.testRoot, "fixtures/", bigFileName);
 
   const smallFileServerPath = path.join(config.filesRoot, smallFileName);
 
   before(function(done) {
-    app = server.listen(3000, done);
-
     checkFileSizeLimit();
 
     createSmallFile();
     createBigFile();
+
+    app = server.listen(3000, done);
+  });
+
+  beforeEach(function() {
+    fs.emptyDirSync(config.filesRoot);
   });
 
   after(function(done) {
-    app.close(done);
     clearFixtures();
+    app.close(done);
   });
 
   describe("GET method", function() {
@@ -58,84 +56,42 @@ describe("Server tests", function() {
       copyTestFile(smallFileTempPath, smallFileServerPath);
     });
 
-    after(function() {
-      deleteTestFile(smallFileServerPath);
-    });
-
     context("when index.html requested", function() {
-      it("should return correctly", async function() {
-        const file = fs.readFileSync("public/index.html");
+      it("should return it correctly", async function() {
+        const file = fs.readFileSync(`${config.publicRoot}/index.html`);
+        const response = await rp(host);
 
-        const response = await rp(`${host}/`);
-
+        response.statusCode.should.be.equal(200);
         response.body.equals(file).should.be.true();
-        
-        
-      }); 
+      });
     });
 
-    context("when file exists", function() {
+    context("otherwise when file exists", function() {
       beforeEach(function() {
+        fs.copySync(
+          `${config.testRoot}/fixtures/small.test`,
+          `${config.filesRoot}/small.test`
+        );
+      });
 
+      it("should return it correctly", async function() {
+        const file = fs.readFileSync(`${config.filesRoot}/small.test`);
+        const response = await rp(`${host}/small.test`);
+
+        response.statusCode.should.be.equal(200);
+        response.body.equals(file).should.be.true();
       });
     });
 
-    it("Should return index.html correctly", function(done) {
-      request("http://localhost:3000/", function(error, response, body) {
-        if (error) return done(error);
-
-        const file = fs.readFileSync("public/index.html", {
-          encoding: "utf-8"
-        });
-        assert.equal(body, file);
-
-        done();
+    context("otherwise when file doesn't exist", function() {
+      it("should return 404 for non-existing but valid filename", async function() {
+        const response = await rp(`${host}/blablabla.test`);
+        response.statusCode.should.be.equal(404);
       });
-    });
 
-    it("Should return 404 for non-existing but valid filename", function(done) {
-      request("http://localhost:3000/blablabla.test", function(
-        error,
-        response
-      ) {
-        if (error) return done(error);
-
-        const expectedCode = 404;
-        assert.equal(expectedCode, response.statusCode);
-
-        done();
-      });
-    });
-
-    it("Should return 400 for invalid filename", function(done) {
-      request("http://localhost:3000/blablabla..test", function(
-        error,
-        response
-      ) {
-        if (error) return done(error);
-
-        const expectedCode = 400;
-        assert.equal(expectedCode, response.statusCode);
-
-        done();
-      });
-    });
-
-    it("Should return existing file correctly", function(done) {
-      request("http://localhost:3000/small.test", function(
-        error,
-        response,
-        body
-      ) {
-        if (error) return done(error);
-
-        const smallFile = fs.readFileSync(smallFileServerPath);
-        const expectedCode = 200;
-
-        assert.equal(expectedCode, response.statusCode);
-        assert.equal(body, smallFile);
-
-        done();
+      it("Should return 400 for invalid filename", async function() {
+        const response = await rp(`${host}/blablabla..test`);
+        response.statusCode.should.be.equal(400);
       });
     });
   });
@@ -151,12 +107,13 @@ describe("Server tests", function() {
 
       bigFile.pipe(
         request(reqOptions, function(error, response) {
-          
           if (error) {
             // see this for description https://github.com/nodejs/node/issues/947#issue-58838888
             // there is a problem in nodejs with it
-            if (error.code === 'ECONNRESET' || error.code === 'EPIPE') {
-              fs.existsSync(config.get('filesRoot') + '/big.png').should.be.false();
+            if (error.code === "ECONNRESET" || error.code === "EPIPE") {
+              fs
+                .existsSync(config.get("filesRoot") + "/big.png")
+                .should.be.false();
               return done();
             } else {
               return done(error);
@@ -267,26 +224,24 @@ describe("Server tests", function() {
 //const createTestFile = (path, size) => fs.writeFileSync(path, new Buffer(size));
 
 const checkFileSizeLimit = () => {
-  if (config.fileSizeLimit > 1e7) 
+  if (config.fileSizeLimit > 1e7)
     throw Error("Should not create very big test-files");
-}
+};
 
 const createSmallFile = () => {
   const fileSize = 1;
   const filePath = `${config.testRoot}/fixtures/small.test`;
   fs.writeFileSync(filePath, Buffer.alloc(fileSize));
-}
+};
 
 const createBigFile = () => {
   const fileSize = config.fileSizeLimit + 1;
   const filePath = `${config.testRoot}/fixtures/big.test`;
   fs.writeFileSync(filePath, Buffer.alloc(fileSize));
-}
+};
 
-const clearFixtures = () => 
+const clearFixtures = () =>
   fs.emptyDirSync(path.join(config.testRoot, "fixtures/"));
-
-
 
 const deleteTestFile = path => fs.unlinkSync(path);
 
